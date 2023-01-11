db6ff99e4fce53423799395ab2e8ea6e91c91c89
Yoookinowww <121599110+Yoookinowww@users.noreply.github.com>
2023-01-11 19:01:09 +0800

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ var http = require('http')
 var request = require('request');
 
 // 这里填你的replit应用的url，多个url用逗号隔开
-let replit_app_urls = ["https://testwebsite.yoookinowww.repl.co","https://lycoreco.yokino.buzz"]
+let replit_app_urls = ["https://testwebsite.yoookinowww.repl.co","https://lycoreco.yokino.buzz","https://link.yokino.buzz"]
 
 var port = 4000
 var server = http.createServer(function (request, response) {
